87857eb4f37f815c4354b2d5fe02b82740d2ba0a
Варвара <varvara@MacBook-Air-Varvara.local>
2025-06-07 13:36:46 +0300

    init

---
+++ src/main/java/io/github/mambichnaya/spamadmin/SpamadminApplication.java
@@ -0,0 +1,13 @@
+package io.github.mambichnaya.spamadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpamadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpamadminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/mambichnaya/spamadmin/SpamadminApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mambichnaya.spamadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpamadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
